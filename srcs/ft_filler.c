d01ccfdce808d43e13b4643765ec6064c055f7ad
Severi <sseveri@gmail.com>
2022-05-02 18:20:21 +0300

    map update reading

--- srcs/ft_filler.c
+++ srcs/ft_filler.c
@@ -6,7 +6,7 @@
 /*   By: severi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 13:38:19 by severi            #+#    #+#             */
-/*   Updated: 2022/05/02 16:23:03 by severi           ###   ########.fr       */
+/*   Updated: 2022/05/02 18:13:08 by severi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
  * @params height
  * @params length
  */
-int **create_map1(int height, int length)
+int **create_map(int height, int length)
 {
 	int **map;
 	int i;
@@ -39,7 +39,7 @@ int **create_map1(int height, int length)
 	{
 		while (j < length)
 		{
-			map[i][j] = 0;
+			map[i][j] = 46;
 			j++;
 		}
 		j = 0;
@@ -49,68 +49,131 @@ int **create_map1(int height, int length)
 }
 
 /*
-int	**create_map(int x)
+ * Create a piece with height x length with "."'s
+ */
+/*
+int **create_piece(int height, int length)
 {
-	int	map1[15][17];
-	int map2[24][40];
-	int	map3[100][99];
+	int **piece;
+	int i;
+	int j;
 
-	//map[0] = [0, 0, 0];
-	//map[1] = [1, 0, 0];
-	//map[2] = [0, 0, 2];
-	if (x == 1)
-		return (map1);
-	else if (x == 2)
-		return (map2);
-	else if (x == 3)
-		return (map3);
-	return (map1);
+	i = 0;
+	j = 0;
+	piece = (int **)malloc(sizeof(int*) * (long unsigned int)height);
+	while (i < height)
+	{
+		piece[i] = (int *)malloc(sizeof(int) * (long unsigned int)length);
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		while (j < length)
+		{
+			piece[i][j] = 46;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (piece);
 }
 */
 
-void print_map(int **map)
+int	**update_map(int **map, char *row)
+{
+	int	j;
+	int i;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (row[i] != ' ')
+		i++;
+	j = ft_atoi(ft_strsub(row, 0, (size_t)i++));
+	while ((size_t)i < ft_strlen(row))
+	{
+		map[j][k++] = row[i++];
+	}
+	return (map);
+}
+
+void print_map(int **map, int fd)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while (i < 25)
+	while (i < 15) // height
 	{
-		while (j < 15)
+		while (j < 17) // length
 		{
-			printf("%d", map[i][j]);
+			ft_putchar_fd((char)(map[i][j]), fd);
+			//printf("%c", map[i][j]);
 			j++;
 		}
 		j = 0;
-		printf("\n");
-		//ft_putchar('\n');
+		//printf("\n");
+		ft_putchar_fd('\n', fd);
 		i++;
 	}
 }
 
+int	get_length(char *str)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (j != 2)
+	{
+		while (str[i] != ' ')
+			i++;
+		i++;
+		j++;
+	}
+	j = 0;
+	while (ft_isdigit(str[i + j]) == 1)
+		j++;
+	return (ft_atoi(ft_strsub(str, (unsigned int)i, (unsigned int)j)));
+}
+
+int	get_height(char *str)
+{
+	int i;
+
+	i = 8;
+	while (ft_isdigit(str[i]) == 1)
+		i++;
+	return (ft_atoi(ft_strsub(str, 8, (size_t)i - 8)));
+}
+
 int	main(void)
 {
-//	char	*buf;
-//	int		fd;
-//	int		read;
-//	char	*found;
-//	t_player	player;
+	char	*buf;
+	int		fd;
+	int		read;
+	char	*found;
+	t_player	player;
 	int		**map;
 
-//	read = 1;
-//	player.player_num = 1;
-//	fd = open("logs.txt", O_WRONLY);
-	map = create_map1(25, 15);
-	print_map(map);
-/*	
+	read = 1;
+	player.player_num = 1;
+	fd = open("logs.txt", O_WRONLY);
+	//map = create_map1(25, 15);
+	//print_map(map);
+	
 	while(1)	
 	{
 		//fd = open("logs.txt", O_WRONLY);
 		//perror("Error printed by perror: ");
 		//ft_putstr_fd("8 2\n", 0);
-		ft_putstr_fd("8 2\n", 1);
-		ft_putnbr(fd);
+		//ft_putstr_fd("8 2\n", 1);
+		//ft_putnbr(fd);
 		//close(fd);
 
 		read = get_next_line(0, &buf);
@@ -122,18 +185,29 @@ int	main(void)
 		{
 			player.player_num = 2;
 		}
-		ft_putchar_fd('\n',fd);
+		found = NULL;
+		//ft_putchar_fd('\n',fd);
 
 		ft_putstr_fd(ft_itoa(player.player_num), fd);
-		ft_putchar_fd('\n',fd);
+		//ft_putchar_fd('\n',fd);
 		free(buf);
+		read = get_next_line(0, &buf);
+		found = ft_strstr(buf, "Plateau");
+		if (found != NULL)
+		{
+			map = create_map(get_height(buf), get_length(buf));
+			print_map(map, fd);
+		}
 		while((read = get_next_line(0, &buf)) != 0)
 		{
 			ft_putstr_fd(buf, fd);
 			ft_putchar_fd('\n',fd);
+			if (ft_isdigit(buf[0]) == 1)
+				update_map(map, buf);
 			free(buf);
 		}
-		
+		print_map(map, fd);	
+
 		//fd = open("logs.txt", O_WRONLY);
 		ft_putstr_fd("8 3\n", 1);
 
@@ -143,6 +217,6 @@ int	main(void)
 		ft_putstr(buf);
 		ft_putchar('\n');
 	}
-*/
+
 	return (0);
 }
